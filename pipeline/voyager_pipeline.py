2cf59fedd8e66cf9af3bde33dc09e06195426736
Collin Wischmcj <cjwischmeyer@gmail.com>
2025-09-24 07:41:41 -0400

    moving paginator and new alerting class to standalone file

--- pipeline/voyager_pipeline.py
+++ pipeline/voyager_pipeline.py
@@ -42,53 +42,7 @@ from configuration.column_mapping import get_map_func
 auth = CustomAuth(username=os.getenv("LINKEDIN_USERNAME"), password=os.getenv("LINKEDIN_PASSWORD"), use_cookie_cache=False)
 auth.authenticate()
 
-def avoid_ban(sleepy_time=1):
-    time.sleep(sleepy_time)
 
-# Helper Class(es)
-
-class LinkedInPaginator(RangePaginator):
-    """
-        This class is used to interface with the LinkedIn API
-        In addition to paginating, it also encodes the url parameters
-            in order to match the LinkedIn API's request format
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.url_template = None
-
-    def encode_params(self, request):
-        """
-            Retrieves a url in the format accepted by 
-                LinkedIn's Voyager API
-        """
-        params = request.json
-        request.json = None
-        endpoint = request.url.split('/')[-1]
-        base_url = request.url.replace(f'/{endpoint}', '')
-        self.url_template = build_gql_url(params,base_url,endpoint)
-
-    def init_request(self, request: Request) -> None:
-        """Generates a rest_li encoded url template based off of 
-            the inputs passed via the requests json
-        """
-        self._has_next_page = True
-        self.current_value = self.initial_value
-        self.encode_params(request)
-        self.update_request(request)
-
-    def update_request(self, request: Request) -> None:
-        """
-            Runs before each request
-            Insert url parameters (namely, start and page count) into url
-        """
-        request.url = self.url_template.substitute(**{self.param_name:self.current_value})
-        print(request.url)
-
-    def update_state(self, response, data):
-        "Runs after each request"
-        super().update_state(response, data)
-        avoid_ban()
 
 # Resource Creation Functions
 # # These are config-driven resource generators
